34a900d8b142ce4ca97b51d3365b455275a1e56d
David Lukac <david.lukac@gmail.com>
2016-06-01 11:35:55 +0200

    Add custom Controller and its templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+//        // replace this example code with whatever you need
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+//        ]);
+        return $this->render('default/index1.html.twig');
     }
+
 }
---
+++ src/AppBundle/Controller/TrainingController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davidlukac
+ * Date: 31/05/16
+ * Time: 11:52
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrainingController extends Controller
+{
+    /**
+     * @Route("/training/{job}/{name}", name="training", defaults={ "name" = "WhoAreYou?" })
+     * @Template()
+     */
+    public function trainingAction($job, $name)
+    {
+        $parameters = [
+            'name' => $name,
+            'job_position' => $job
+        ];
+
+        return $parameters;
+    }
+
+}
